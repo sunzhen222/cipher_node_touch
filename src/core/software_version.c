51c52eba809909013f70e38e7317c1a2f9f5edbd
Leon <sunzhen_222@163.com>
2026-06-16 22:37:21 +0800

    update software version

--- src/core/software_version.c
+++ src/core/software_version.c
@@ -2,8 +2,8 @@
 #include "stdio.h"
 
 #define SOFTWARE_VERSION_MAJOR      1
-#define SOFTWARE_VERSION_MINOR      1
-#define SOFTWARE_VERSION_BUILD      1
+#define SOFTWARE_VERSION_MINOR      2
+#define SOFTWARE_VERSION_BUILD      0
 
 const char *GetSoftwareVersionString(void)
 {
